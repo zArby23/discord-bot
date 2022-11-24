aad2ebf3cfd3367604635f3524aa50e4b8c44fff
zArby23 <lic.jdavid23@gmail.com>
2022-11-24 18:43:05 -0500

    Created a command

---
+++ commands/test.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+            .setName('test')
+            .setDescription('A command created for test purposes, it just replies with something...'),
+    async execute(interaction){
+        await interaction.reply("Surprise! I'm working");
+    },
+}

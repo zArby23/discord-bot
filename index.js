32ad2e4ffdf76b6c32e14463f91ecb1ee359cfad
zArby23 <lic.jdavid23@gmail.com>
2022-11-25 18:20:10 -0500

    Removed the events (Changed the location of them)

--- index.js
+++ index.js
@@ -2,22 +2,36 @@ const fs = require('node:fs');
 const path = require('node:path');
 // const Discord = require("discord.js");
 const config = require("./config.json");
+
 const {Client, Events, Collection, GatewayIntentBits} = require('discord.js');
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages
     ]
 });
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, 'events');
+const eventsFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandsFiles){
+for (const file of eventsFiles){
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+
+    if (event.once){
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
     const filePath = path.join(commandsPath, file)
     const command = require(filePath);
 
@@ -27,23 +41,5 @@ for (const file of commandsFiles){
         console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
     }
 }
-
-client.on(Events.InteractionCreate, async interaction => {
-    const command = interaction.client.comands.get(interaction.commandName);
-
-    if(!command){
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error){
-        console.error(error);
-        await interaction.reply({
-            content: 'There was an error while executing the command!', ephemeral: true
-        });
-    }
-})
 client.login(config.BOT_TOKEN)
 
